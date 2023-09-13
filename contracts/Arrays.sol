844e9635d0d47d0ad687de40a84eb8c3fd2e5305
LasseJ <lasse.juusela@gmail.com>
2023-09-13 11:42:31 +0300

    Arrays

--- contracts/Arrays.sol
+++ contracts/Arrays.sol
@@ -11,7 +11,13 @@ contract Arrays1 {
     uint[10] public array3; // this initializes a fixed size array of 10 uints, default to 0
     string[] public array4  = ["apple", "banana", "carrot"];
     string[] public array5; // this initializes an array of strings
-    string[10] public array6; // this initializes a fixed size array of 10 uints, default to ""
+    string[10] public array6; // this initializes a fixed size array of 10 strings, default to ""
+    address[] public array7 = [0xe5c430b2Dd2150a20f25C7fEde9981f767A48A3c, 0xE5C430b2dD2150a20f25C7fEdE9981f767A48a3D, 0xe5C430B2dD2150A20f25c7FedE9981f767A48a3E];
+    address[] public array8;
+    address[10] public array9;
+    bool[] public array10 = [true, false, true];
+    bool[] public array11;
+    bool[10] public array12; // default to false
 }
 
 
@@ -30,6 +36,7 @@ contract Arrays2 {
         return array;
     }
 
+    // Return the length of the array
     function length() public view returns (uint) {
         return array.length;
     }
@@ -47,5 +54,39 @@ contract Arrays2 {
     // Remove item at given index
     function remove(uint index) public {
         delete array[index];
+    }
+}
+
+contract Arrays3 {
+    string[] public stringArray;
+
+    // Get value at a given index
+    function get(uint i) public view returns (string memory) {
+        return stringArray[i];
+    }
+
+    // Return the whole array
+    function getArray() public view returns (string[] memory) {
+        return stringArray;
+    }
+
+    // Return the length of the array
+    function length() public view returns (uint) {
+        return stringArray.length;
+    }
+
+    // Adds new value at the end of the array
+    function push(string memory i) public {
+        stringArray.push(i);
+    }
+
+    // Removes last item from array
+    function pop() public {
+        stringArray.pop();
+    }
+
+    // Remove item at given index
+    function remove(uint index) public {
+        delete stringArray[index];
     }
 }
--- test/Arrays.js
+++ test/Arrays.js
@@ -30,6 +30,29 @@ describe('Arrays', () => {
       expect(await contract.array6(0)).to.equal('')
       expect(await contract.array6(1)).to.equal('')
       expect(await contract.array6(9)).to.equal('')
+
+      expect(await contract.array7(0)).to.equal('0xe5c430b2Dd2150a20f25C7fEde9981f767A48A3c')
+      expect(await contract.array7(1)).to.equal('0xE5C430b2dD2150a20f25C7fEdE9981f767A48a3D')
+      expect(await contract.array7(2)).to.equal('0xe5C430B2dD2150A20f25c7FedE9981f767A48a3E')
+
+      // Note: this will fail because it's not initialized with default values
+      // expect(await contract.array8(0)).to.equal(0)
+
+      expect(await contract.array9(0)).to.equal('0x0000000000000000000000000000000000000000')
+      expect(await contract.array9(1)).to.equal('0x0000000000000000000000000000000000000000')
+      expect(await contract.array9(9)).to.equal('0x0000000000000000000000000000000000000000')
+
+      expect(await contract.array10(0)).to.equal(true)
+      expect(await contract.array10(1)).to.equal(false)
+      expect(await contract.array10(2)).to.equal(true)
+
+      // Note: this will fail because it's not initialized with default values
+      // expect(await contract.array11(0)).to.equal(0)
+
+      expect(await contract.array12(0)).to.equal(false)
+      expect(await contract.array12(1)).to.equal(false)
+      expect(await contract.array12(9)).to.equal(false)
+
     })
   })
 
@@ -72,4 +95,42 @@ describe('Arrays', () => {
     })
   })
 
+  describe('Example 3', () => {
+
+    it('demonstrates functions', async () => {
+      const Contract = await ethers.getContractFactory('Arrays3')
+      let contract = await Contract.deploy()
+
+      // Array is empty at the start
+      expect(await contract.length()).to.equal(0)
+
+      // Add some items
+      await contract.push('apple')
+      await contract.push('banana')
+      await contract.push('carrot')
+
+      // Check length after
+      expect(await contract.length()).to.equal(3)
+
+      // Get the whole array
+      let stringArray = await contract.getArray()
+      expect(stringArray[0]).to.equal('apple')
+      expect(stringArray[1]).to.equal('banana')
+      expect(stringArray[2]).to.equal('carrot')
+
+      // Read items from contract
+      expect(await contract.stringArray(0)).to.equal('apple')
+      expect(await contract.get(0)).to.equal('apple')
+
+      // Remove last item
+      await contract.pop()
+      expect(await contract.length()).to.equal(2)
+
+      // Delete first item
+      await contract.remove(0)
+      expect(await contract.length()).to.equal(2) // preserves length
+      expect(await contract.stringArray(0)).to.equal("") // resets to default value
+
+    })
+  })
 })
